77e2daec79ea020321c0e9ed8e300946c3896fcf
Antoine Cerdan <antoinecerdan31@gmail.com>
2023-11-19 21:00:41 +0100

    flags, title, base colors and fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import Card from './components/Card'
+import Desk from './components/Desk'
+import Footer from './components/Footer'
+
 import './App.css'
 
 function App() {
 
   return (
     <div className="app">
+        <Desk></Desk>
         <Card></Card>
+        <Footer></Footer>
     </div>
   )
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,15 @@
 import './Card.css'
+import lesbian from "../assets/lesbian.svg"
+import trans from "../assets/trans.svg"
 
 export default function Card(){
     return (
-        <div>
-            hey
+        <div className='card'>
+            <p className="name">eosis</p>
+            <p className="about"><b>web dev and CS enjoyer</b></p>
+            <p className="personnal">19 yo / INFP / <span><img src={trans} className='flag'/> <img src={lesbian} className='flag'/></span></p>
+            <div className="socials"></div>
+            <p className="email">hey@eosis.space</p>
         </div>
     )
 }
