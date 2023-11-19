f2410130e8d5856945428bc3d501fedfd82d37a1
Antoine Cerdan <antoinecerdan31@gmail.com>
2023-11-19 23:09:14 +0100

    socials, and card done

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,12 @@
 import './Card.css'
 import lesbian from "../assets/lesbian.svg"
 import trans from "../assets/trans.svg"
+import github from "../assets/github.svg"
+import twitter from "../assets/twitter.svg"
+import youtube from "../assets/youtube.svg"
+import steam from "../assets/steam.svg"
+import twitch from "../assets/twitch.svg"
+import kofi from "../assets/kofi.svg"
 
 export default function Card(){
     return (
@@ -8,7 +14,14 @@ export default function Card(){
             <p className="name">eosis</p>
             <p className="about"><b>web dev and CS enjoyer</b></p>
             <p className="personnal">19 yo / INFP / <span><img src={trans} className='flag'/> <img src={lesbian} className='flag'/></span></p>
-            <div className="socials"></div>
+            <div className="socials">
+                <img src={github} className='logo' />
+                <img src={twitter} className='logo' />
+                <img src={youtube} className='logo' />
+                <img src={steam} className='logo' />
+                <img src={twitch} className='logo' />
+                <img src={kofi} className='logo' />
+            </div>
             <p className="email">hey@eosis.space</p>
         </div>
     )
--- src/components/Desk.jsx
+++ src/components/Desk.jsx
@@ -2,6 +2,8 @@ import "./Desk.css"
 
 export default function Desk(){
     return (
-        <></>
+        <div className="desk">
+            <div className="obj">desk.obj</div>
+        </div>
     )
 }
