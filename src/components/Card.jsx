743596711294d7c0f2fedfb3ab5629125d6aa262
Antoine Cerdan <antoinecerdan31@gmail.com>
2023-11-21 13:09:26 +0100

    icon, removed useless files and made socials.js

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,26 +1,20 @@
 import './Card.css'
 import lesbian from "../assets/lesbian.svg"
 import trans from "../assets/trans.svg"
-import github from "../assets/github.svg"
-import twitter from "../assets/twitter.svg"
-import youtube from "../assets/youtube.svg"
-import steam from "../assets/steam.svg"
-import twitch from "../assets/twitch.svg"
-import kofi from "../assets/kofi.svg"
+
+import socials from "../socials.js"
 
 export default function Card(){
+    const socialsElements = socials.map((social) => {
+        return <a key={social.id} href={social.link} target='_blank' rel='noreferrer'><img src={social.logo} className='logo'></img></a>
+    })
     return (
         <div className='card'>
             <p className="name">eosis</p>
             <p className="about"><b>web dev and CS enjoyer</b></p>
             <p className="personnal">19 yo / INFP / <span><img src={trans} className='flag'/> <img src={lesbian} className='flag'/></span></p>
             <div className="socials">
-                <a href="https://github.com/eosiswastaken" target='_blank' rel="noreferrer"><img src={github} className='logo' ></img></a>
-                <a href="https://twitter.com/zoermdr" target='_blank' rel="noreferrer"><img src={twitter} className='logo' ></img></a>
-                <a href="https://youtube.com/@eosiswastaken" target='_blank' rel="noreferrer"><img src={youtube} className='logo' ></img></a>
-                <a href="https://steamcommunity.com/id/eosiswastaken/" target='_blank' rel="noreferrer"><img src={steam} className='logo' ></img></a>
-                <a href="https://twitch.com/eosis_" target='_blank' rel="noreferrer"><img src={twitch} className='logo' ></img></a>
-                <a href="https://ko-fi.com/eosiswastaken" target='_blank' rel="noreferrer"><img src={kofi} className='logo' ></img></a>
+                {socialsElements}
             </div>  
             <a className="email" href="mailto:hey@eosis.space">hey@eosis.space</a>
         </div>
---
+++ src/socials.js
@@ -0,0 +1,38 @@
+export default [
+    {
+        id: 1,
+        link: "https://github.com/eosiswastaken",
+        logo:"github.svg",
+        alias: "github"
+    },
+    {
+        id: 2,
+        link: "https://twitter.com/zoermdr",
+        logo:"twitter.svg",
+        alias: "twitter"
+    },
+    {
+        id: 3,
+        link: "https://youtube.com/@eosiswastaken",
+        logo:"youtube.svg",
+        alias: "youtube"
+    },
+    {
+        id: 4,
+        link: "https://steamcommunity.com/id/eosiswastaken/",
+        logo:"steam.svg",
+        alias: "steam"
+    },
+    {
+        id: 5,
+        link: "https://twitch.com/eosis_",
+        logo:"twitch.svg",
+        alias: "twitch"
+    },
+    {
+        id: 6,
+        link: "https://ko-fi.com/eosiswastaken",
+        logo:"kofi.svg",
+        alias: "kofi"
+    }
+]
