2b115eba93b81f2ce66ecafa544a6fda381c23cc
Antoine Cerdan <antoinecerdan31@gmail.com>
2023-12-01 21:30:21 +0100

    analytics

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import Card from './components/Card'
 import Desk from './components/Desk'
 import Footer from './components/Footer'
 import './App.css'
-
+import { inject } from '@vercel/analytics';
+ 
 function App() {
 
+  inject();
   return (
     <div className="app">
         <Desk></Desk>
